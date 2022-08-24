5dda306cd6903912cd6dbfee514d9b7bd666463d
Laura Greenstreet <laura.greenstreet@gmail.com>
2022-08-23 19:43:48 -0700

    Added Slide-seq and bacterial colony reconstructions

--- functions/simulate_barcodes.py
+++ functions/simulate_barcodes.py
@@ -1,5 +1,4 @@
 import time
-
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import euclidean_distances
@@ -8,27 +7,16 @@ import anndata
 import scipy
 from tqdm import tqdm
 import numexpr as ne
-import utils
+import visuals
 
-#Given amplitude and variance return a corresp.
-#gaussian.
+# Given amplitude and variance return the corresponding Gaussian.
 def make_gauss_kernel(a, var):
     def gauss(x2):
         return a*np.exp(-x2/(2*var))
 
     return gauss
 
-#Given a standard deviation and a random
-#number generator, makes a normally dist.
-#noise function with that standard deviation.
-def make_noise_kernel(std, rng):
-    def noise(x):
-        return rng.normal(x, std)
-
-    return noise
-
-# Given a distribution on reads, returns a vector
-# of reads of length N_beads
+# Given a distribution on reads, returns a vector of reads of length N_beads.
 def distribute_reads(reads_dist, mean, N_beads):
     # Get a vector of qualities
     first_sample = np.random.choice(reads_dist['reads'], size=N_beads, p=reads_dist['p'])
@@ -45,9 +33,8 @@ def distribute_reads(reads_dist, mean, N_beads):
 
     return N_reads
 
-# Given a barcodes distribution, resamples it for
-# num_needed barcodes. Ie. creates a new distribution
-# with num_needed unique barcodes.
+# Given a barcodes distribution, resamples it for num_needed barcodes,
+# ie. creates a new distribution with num_needed unique barcodes.
 def resample_overlaps(barcodes_dist, num_needed):
     # Sort the existing distribution and fit a spline to it
     sorted_dist = np.sort(barcodes_dist)
@@ -64,21 +51,21 @@ def resample_overlaps(barcodes_dist, num_needed):
     return new_probs
 
 
-#Computes colony counts matrix for a subset of indices
-#PARAMS:
-#sigma: The standard deviation for the Gaussian
-#beads_coords: The coordinates of the beads
-#colonies_coords: The coordinates of the colonies
-#sub_indices: The indices of the beads that will be included in the counts matrix (optional)
-#overlap_dist: An array of probabilities for each rank of barcode to be drawn.
-#reads_dist: A pd dataframe with two columns 'reads' and 'p', where p gives the probability of each count
-#            occuring
-#scaling: Boolean for whether or not to scale the distribution to a mean number of rpbs
-#overlap_scaling: Whether we want to resample our overlap distribution (ussually to have more complexity)
-#rpb: The mean number of rpbs if scaling or if no distribution is provided
-#rng: A numpy random number generator (optional)
-#num_proc: The number of processors to use
-#chunksize: The number of beads each cpu computes at a time. Too low a chunksize will severely impact performance.
+# Computes colony counts matrix for a subset of indices.
+# PARAMS:
+# -- sigma: The standard deviation for the Gaussian kernel
+# -- beads_coords: The coordinates of the beads
+# -- colonies_coords: The coordinates of the colonies
+# -- sub_indices: The indices of the beads that will be included in the counts matrix (optional)
+# -- overlap_dist: An array of probabilities for each rank of barcode to be drawn.
+# -- reads_dist: A pd dataframe with two columns 'reads' and 'p', where p gives the probability of each count
+#                occuring
+# -- scaling: Boolean for whether or not to scale the distribution to a mean number of rpbs
+# -- overlap_scaling: Whether we want to resample our overlap distribution (ussually to have more complexity)
+# -- rpb: The mean number of rpbs if scaling or if no distribution is provided
+# -- rng: A numpy random number generator for reproducibility (optional)
+# -- num_proc: The number of processors to use
+# -- chunksize: The number of beads each cpu computes at a time. Too low a chunksize will severely impact performance.
 def get_colony_counts(sigma, beads_coords, colonies_coords,
                       sub_indices=None, scaling=False, overlap_scaling=False,
                       rpb=1000, overlap_dist=None, overlap_complexity=None, reads_dist=None,
@@ -91,21 +78,18 @@ def get_colony_counts(sigma, beads_coords, colonies_coords,
     col_ind_l = []
     vals_l = []
 
-    #Check if we need to make a new random number generator
+    # Check if we need to make a new random number generator
     if rng is None:
         SEED = 42 # Random seed for reproducibility
         rng = np.random.default_rng(SEED) # Make rng instance with the seed
 
-    gaussian_count_ker = make_gauss_kernel(1, sigma**2) #Make the Gaussian function for counts
+    gaussian_count_ker = make_gauss_kernel(1, sigma**2) # Make the Gaussian function for counts
 
-#     if stdev_noise > 0:
-#         noise_kernel = make_noise_kernel(stdev_noise, rng) #Make the Gaussian dist. for noise
-
-    #Set which bead indices we'll be considering
+    # Set which bead indices we'll be considering
     if sub_indices is None:
         sub_indices = range(0, beads_coords.shape[0])
 
-    #Get the number of beads
+    # Get the number of beads
     N = len(sub_indices)
 
     # Get the number of rpb for each bead
@@ -137,10 +121,6 @@ def get_colony_counts(sigma, beads_coords, colonies_coords,
         dists_below_thresh = sqd[col_ind] # Get the distances below the threshold
         counts_dist = gaussian_count_ker(dists_below_thresh)  # Compute raw counts values
 
-#         if stdev_noise > 0:
-#             counts_dist = noise_kernel(counts_dist)  # Add noise to counts
-#             counts_dist = counts_dist[counts_dist > 0]  # Make sure no counts are below zero
-
         # Normalize so that the counts distribution is a real distribution
         sum_vals = ne.evaluate('sum(counts_dist)')
         counts_dist = counts_dist/sum_vals
@@ -148,7 +128,7 @@ def get_colony_counts(sigma, beads_coords, colonies_coords,
         # Perform sampling
         reads_bead = N_rpb[i] # The number of reads for this bead
 
-        #Check if our data is too sparse to produce counts
+        # Check if our data is too sparse to produce counts
         if len(counts_dist) == 0:
             return [[], [], []]
 
@@ -183,7 +163,7 @@ def get_colony_counts(sigma, beads_coords, colonies_coords,
 
     print("Matrix build time:",time.time()-t0)
 
-    #Concat all the counts
+    # Concatenate all the counts
     colony_counts = scipy.sparse.csr_matrix((np.concatenate(vals_l), (np.concatenate(row_ind_l), np.concatenate(col_ind_l))), shape=(N,len(overlap_dist)), dtype=np.int16)
     colony_counts.eliminate_zeros()
 
@@ -196,10 +176,11 @@ def get_colony_counts(sigma, beads_coords, colonies_coords,
 
 
 # Creates a kernel that accepts n x d matrices (n samples in dimension d).
-# amp is the amplitude
-# theta: angle (in radians) of the anisotropy
-# sigma: diffusion level of the first direction (pointing at the angle theta)
-# anisotropy: strength of the anisotropy: how much less the second direction is diffused. A value of 1 means isotropic.
+# PARAMS:
+#  -- amp is the amplitude
+#  -- theta: angle (in radians) of the anisotropy
+#  -- sigma: diffusion level of the first direction (pointing at the angle theta)
+#  -- anisotropy: strength of the anisotropy: how much less the second direction is diffused. A value of 1 means isotropic.
 def make_gauss_kernel_uniform_anisotropic(amp=1, theta=0, sigma=0.1, anisotropy=2.0):
     # https://janakiev.com/blog/covariance-matrix/
     # https://en.wikipedia.org/wiki/Multivariate_normal_distribution
@@ -306,13 +287,6 @@ def get_colony_counts_anisotropic_diffusion(sigma, beads_coords, colonies_coords
 
     t0 = time.time()
 
-    # # Progress bar works, and same timing as without tqdm and with pathos.
-    # import tqdm
-    # import pathos.multiprocessing as mp
-    # with mp.Pool(num_proc) as p:
-    #     results = list(tqdm.tqdm(p.imap(worker_colony_counts, range(len(sub_indices)), chunksize=chunksize),
-    #                              total=len(sub_indices)))
-
     results = []
     for i in range(len(sub_indices)):
         results.append(worker_colony_counts(i))
@@ -339,20 +313,20 @@ def get_colony_counts_anisotropic_diffusion(sigma, beads_coords, colonies_coords
     return colony_counts
 
 # Generates an anndata whose read matrix contains satellite barcode counts.
-#PARAMS:
-# sigma: The standard deviation for the Gaussian
-# beads_coords: The coordinates of the beads
-# colonies_coords: The coordinates of the colonies
-# sub_indices: The indices of the beads that will be included in the counts matrix (optional)
-# overlap_dist: An array of probabilities for each rank of barcode to be drawn.
-# reads_dist: A pd dataframe with two columns 'reads' and 'p', where p gives the probability of each count
-#            occuring
-# scaling: Boolean for whether or not to scale the distribution to a mean number of rpbs
-# overlap_scaling: Whether we want to resample our overlap distribution (ussually to have more complexity)
-# rpb: The mean number of rpbs if scaling or if no distribution is provided
-# rng: A numpy random number generator (optional)
-# num_proc: The number of processors to use
-# chunksize: The number of beads each cpu computes at a time. Too low a chunksize will severely impact performance.
+# PARAMS:
+# -- sigma: The standard deviation for the Gaussian
+# -- beads_coords: The coordinates of the beads
+# -- colonies_coords: The coordinates of the colonies
+# -- sub_indices: The indices of the beads that will be included in the counts matrix (optional)
+# -- overlap_dist: An array of probabilities for each rank of barcode to be drawn.
+# -- reads_dist: A pd dataframe with two columns 'reads' and 'p', where p gives the probability of each count
+#                occuring
+# -- scaling: Boolean for whether or not to scale the distribution to a mean number of rpbs
+# -- overlap_scaling: Whether we want to resample our overlap distribution (ussually to have more complexity)
+# -- rpb: The mean number of rpbs if scaling or if no distribution is provided
+# -- rng: A numpy random number generator (optional)
+# -- num_proc: The number of processors to use
+# -- chunksize: The number of beads each cpu computes at a time. Too low a chunksize will severely impact performance.
 def generate_reads(sigma, bead_coords, satellite_coords, sub_indices=None, overlap_dist=None, overlap_complexity=None, reads_dist=None,
                    num_proc=32, rng=None, chunksize=30000, scaling=False):
     satellite_counts = get_colony_counts(sigma, bead_coords, satellite_coords, num_proc=num_proc,
@@ -363,6 +337,6 @@ def generate_reads(sigma, bead_coords, satellite_coords, sub_indices=None, overl
 
     adata = anndata.AnnData(X=satellite_counts, obs=beads_df, var=pd.DataFrame(index=range(satellite_counts.shape[1])), dtype=np.int32)
 
-    adata.obs['spatial_color'] = utils.radial_color(adata.obs.x, adata.obs.y, x0=0.5, y0=0.5)
+    adata.obs['spatial_color'] = visuals.radial_color(adata.obs.x, adata.obs.y, x0=0.5, y0=0.5)
 
     return adata
---
+++ functions/visuals.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import sys
+import os
+import numpy as np
+import math
+from colormap import rgb2hex
+from colorutils import Color
+
+# Given vectors of x and y coordinates returns a color gradient where x ranges over
+# red and y ranges over red, resulting in colors from black to purple.
+def euclidean_color(x, y):
+    x = (x - min(x))/(max(x) - min(x))
+    y = (y - min(y))/(max(y) - min(y))
+    return [rgb2hex(int(255*xi), 0, int(255*yi)) for xi, yi in zip(x,y)]
+
+# Given vectors of x and y coordinates and the center point returns colors by angle with
+# contour lines every 5% of the max radius
+def radial_color(x, y, x0=0, y0=0):
+    spatial_coords_polar = np.array([np.arctan2(y - y0, x - x0), np.sqrt((y - y0)**2 + (x - x0)**2)]).T
+
+    max_r = max(spatial_coords_polar[:,1])
+    def polar_rainbow(theta, r):
+        if int(100*r/max_r) % 10 == 0:
+            return '#000000'
+        else:
+            return Color(hsv=(360*(theta+math.pi)/(2*math.pi), 0.8, 0.8)).hex
+
+    polar_rainbow_vec = np.vectorize(polar_rainbow)
+    return polar_rainbow_vec(spatial_coords_polar[:,0], spatial_coords_polar[:,1])
+
+# Given the ground truth and aligned coordinates, produces three plots showing
+# the ground truth, the reconstruction, and the reconstruction colored by distance
+# to the ground truth.
+# PARAMS:
+# -- adata: anndata containing the ground truth coordinates in obs
+# -- aligned_coords: reconstruction coordinates after the affine transformation
+# -- dist_gt: distance to the ground truth for each bead
+def ground_truth_comparison(adata, aligned_coords, dist_gt):
+    fig = plt.figure(figsize=(12,4))
+    gs = fig.add_gridspec(1,16)
+    plt.subplot(gs[:,:5])
+    plt.title('Ground Truth')
+    plt.scatter(adata.obs.x, adata.obs.y, c=adata.obs.spatial_color, s=1)
+    plt.axis('off')
+
+    plt.subplot(gs[:,5:10])
+    plt.title('Reconstruction')
+    indices = np.arange(adata.shape[0])
+    np.random.shuffle(indices)
+    plt.scatter(aligned_coords[indices][:,0], aligned_coords[indices][:,1], c=adata[indices].obs.spatial_color, s=1)
+    plt.axis('off')
+
+    plt.subplot(gs[:,10:])
+    plt.title('Accuracy (median = {:.2f} $\mu m$)'.format(np.median(dist_gt)))
+    plt.scatter(aligned_coords[indices][:,0], aligned_coords[indices][:,1], c=dist_gt, s=1, vmax=100)
+    plt.colorbar(label='Distance to Ground Truth ($\mu m$)', orientation = 'vertical')
+    plt.axis('off')
+
+    plt.show()
+
+# Given a vector of distances to the ground truth and a maximum distance produces
+# a complementary cummulative distribution plot with horizontal lines for the 50th
+# and 90th percentiles.
+def complementary_cummulative_distribution(dist_gt, max_dist):
+    bins = np.arange(0, max_dist, 1)
+    bins = np.append(bins, max(max(dist_gt), max_dist))
+    hist = plt.hist(dist_gt, bins=bins, density=True, cumulative=True)
+    vals = np.insert(hist[0],0,0)
+    plt.close()
+
+    fig = plt.figure(figsize=(4,4))
+    plt.title('Complementary Cummulative Distribution ($P(x \leq d)$)')
+    plt.plot(bins, 1 - vals)
+    plt.ylabel('Proportion')
+    plt.xlabel('Distance (d) to Ground Truth ($\mu m$)')
+
+    plt.xlim([0,max_dist])
+    plt.ylim([0,1])
+    xticks = np.arange(0,max_dist+1,10)
+    xlabels = ['{}+'.format(t) if t == max(xticks) else '{}'.format(t) if t % 20 == 0 else '' for t in xticks]
+    plt.xticks(ticks=xticks, labels=xlabels)
+
+    plt.axhline(0.1, c='k', linestyle='dashed', linewidth=0.5, zorder=0)
+    plt.axhline(0.5, c='k', linestyle='dashed', linewidth=0.5, zorder=0)
+    plt.show()
